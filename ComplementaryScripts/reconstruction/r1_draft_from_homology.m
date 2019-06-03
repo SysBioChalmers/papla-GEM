7c4f17eba5c81802389e2c40c772d5c9af70cbea
Eduard Kerkhoven <eduardk@chalmers.se>
2019-06-03 11:29:24 +0200

    feat: reconstruction based on homology with R. toruloides
    - rhto-GEM version 1.1.1 is used, should be updated when a new version becomes available.

--- ComplementaryScripts/reconstruction/r1_draft_from_homology.m
+++ ComplementaryScripts/reconstruction/r1_draft_from_homology.m
@@ -1,2 +1,45 @@
-%% This script generates a first draft of a genome-scale model (here: P.
-% laurentii) using the getModelFromHomology function in RAVEN.
+%% This script generates a first draft of a genome-scale model for P. laurentii (papla)
+% Prepare some redirects to folders in the repository
+root  = regexprep(pwd(),'(.*)\\[^\\]*\\.*','$1');
+scripts = [root '/ComplementaryScripts'];
+data    = [root '/ComplementaryData'];
+
+% BLAST against R. toruloides genome. Rhto protein fasta obtained from JGI, modified
+% to have genes in format RHTO_00001. Papla protein fasta obtained from UFV.
+blastRhto = getBlast('papla',[data '/genome/Papla_protein.fasta'], ...
+            'rhto',[data '/genome/rhto_np11.faa']);
+% Save intermediate files in 'scrap' folder that is not tracked by Git
+mkdir([root '/scrap'])
+save([root '/scrap/blastStruct.mat'],'blast*');
+
+% Load R. toruloides model version 1.1.1 (UPDATE WITH NEWER VERSION WHEN
+% AVAILABLE!), downloaded from
+% https://github.com/SysBioChalmers/rhto-GEM/blob/master/ModelFiles/xml/rhto.xml
+modelRhto=importModel([data,'/reconstruction/rhto.xml'],true);
+% Check that the model is functional:
+sol=solveLP(modelRhto)
+
+% Generate draft model, based on homology:
+model=getModelFromHomology(modelRhto,blastRhto,'papla',{},1,false,10^-20,150,35);
+
+% Add meta data
+model.annotation.defaultLB    = -1000;
+model.annotation.defaultUB    = +1000;
+model.annotation.taxonomy     = 'taxonomy/5418'; % NCBI taxonomy ID
+model.annotation.givenName    = 'Eduard';
+model.annotation.familyName   = 'Kerkhoven';
+model.annotation.email        = 'eduardk@chalmers.se';
+model.annotation.organization = 'Chalmers University of Technology';
+model.annotation.note         = 'Papiliotrema laurentii UFV-1';
+model.id                      = 'papla';
+model.description             = 'Papiliotrema laurentii-GEM_v0.0.1';
+
+save([root '/scrap/model_r1.mat'],'model');
+% Export to Excel format for easy inspection
+exportToExcelFormat(model,[root '/scrap/papla_v0.0.1.xlsx']);
+% To prepare files for commit to GitHub, run newCommit function. This
+% writes the necessary models files (xml, yml, txt) etc.
+cd(scripts); newCommit(model); cd('reconstruction');
+
+
+
