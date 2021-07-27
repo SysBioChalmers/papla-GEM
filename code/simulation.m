04679e9f4869790c655c6fd8d4fbb275a91e7c08
Eduard Kerkhoven <eduardk@chalmers.se>
2021-07-27 13:37:36 +0200

    refactor: split reconstruction and simulation

---
+++ code/simulation.m
@@ -0,0 +1,155 @@
+%% SIMULATIONS
+% Load model
+model = importModel('../model/papla-GEM.xml');
+
+%% Validate with rates measured in this study
+
+%Load data:
+fid         = fopen('../data/biomass/bioreactor_growth.csv');
+fluxData    = textscan(fid,'%f32 %f32 %s','Delimiter',',','HeaderLines',1);
+cSourceData = fluxData{3};
+fluxData    = [fluxData{1} fluxData{2}];
+fclose(fid);
+
+% Yeast extract in medium, allow uptake of amino acids
+aminoacidRxns = {'r_1810'; ... % L-glycine
+                 'r_1873'; ... % L-alanine
+                 'r_1879'; ... % L-arginine
+                 'r_1880'; ... % L-asparagine
+                 'r_1881'; ... % L-aspartate
+                 'r_1883'; ... % L-cysteine
+                 'r_1889'; ... % L-glutamate
+                 'r_1891'; ... % L-glutamine
+                 'r_1893'; ... % L-histidine
+                 'r_1897'; ... % L-isoleucine
+                 'r_1899'; ... % L-leucine
+                 'r_1900'; ... % L-lysine
+                 'r_1902'; ... % L-methionine
+                 'r_1903'; ... % L-phenylalanine
+                 'r_1904'; ... % L-proline
+                 'r_1905'; ... % L-serine
+                 'r_1911'; ... % L-threonine
+                 'r_1912'; ... % L-tryptophan
+                 'r_1913'; ... % L-tyrosine
+                 'r_1914'};    % L-valine              
+modelTmp = setParam(model, 'lb', aminoacidRxns, -0.1);
+
+disp('Growth on glucose:')
+for i=1:4
+    modelTmp = setParam(modelTmp,'lb','r_1714',-fluxData(i,2));
+    sol = solveLP(modelTmp,1);
+    fprintf('Growth rate measured: %.4f / simulated: %.4f\n',fluxData(i,1),-sol.f)
+end
+disp('Growth on xylose:')
+modelTmp = setParam(modelTmp,'lb',{'r_1714','r_1718'},[0,-fluxData(5,2)]);
+sol = solveLP(modelTmp,1);
+fprintf('Growth rate measured: %.4f / simulated: %.4f\n',fluxData(5,1),-sol.f)
+
+%% Simulate different carbon sources
+% set the carbon source and unlimited O2 for aerobic growth
+cSource = {'glucose','acetate','galactose','sucrose','xylose',...
+    'galacturonate','glyerol','L-arabinose','D-arabinose','mannose',...
+    '(R)-lactate','(S)-lactate'};
+exchRxn = {'r_1714','r_1634','r_1710','r_2058','r_1718','r_1711',...
+    'r_1808','r_1878','r_1706','r_1715','r_1546','r_1551'};
+zeroFlx = repmat(0,1,numel(cSource));
+
+model = setParam(model, 'lb', {'r_1992'}, -1000);    % O2
+model = setParam(model, 'ub', {'r_1992'}, 0);
+% set biomass pseudoreaction as objective
+model = setParam(model, 'obj',{'r_2111'}, 1);   
+
+% Loop through all carbon sources and print exchange fluxes
+for i = 1:numel(cSource)
+    fprintf('\nSimulate growth on %s:\n', cSource{i})
+    modelTmp = setParam(model,'lb',exchRxn,zeroFlx);
+    modelTmp = setParam(modelTmp,'lb',exchRxn{i},-10);
+    sol = solveLP(modelTmp);
+    printFluxes(modelTmp,sol.x)
+end
+
+%%  OLEAGINOUS FBA
+%  Add exchange reactions for triglyceride (16:0/18:1/18:1-TAG).
+idx = getIndexes(model, {'triglyceride (1-16:0, 2-18:1, 3-18:1)[erm]', ...
+    'triglyceride (1-16:0, 2-18:1, 3-18:1)[lp]'}, 'metscomps');
+% Add exchange reactions for products
+rxnsToAdd.rxns          = 'exch_TAG';
+rxnsToAdd.mets          = model.mets(idx);
+rxnsToAdd.stoichCoeffs  = {[-1, -1]}; 
+rxnsToAdd.lb            = 0;
+model = addRxns(model,rxnsToAdd);
+
+model = setParam(model,'obj','exch_TAG',1);
+model = setParam(model,'lb','r_4046',0);
+sol=solveLP(model,1)
+printFluxes(model,sol.x)
+
+% Make sure that COBRA Toolbox version >3 is installed
+initCobraToolbox(false)
+
+model       = setParam(model, 'eq', {'r_1714', 'r_1718'}, [-1, 0]);
+Glc.grRatio = singleRxnDeletion(model,'FBA');
+
+modelXyl    = setParam(model, 'eq', {'r_1714', 'r_1718'}, [0, -1]);
+Xyl.grRatio = singleRxnDeletion(modelXyl,'FBA');
+
+
+idx = find(Glc.grRatio < 0.90 | Xyl.grRatio < 0.90);
+
+out = [num2cell(Glc.grRatio(idx)*100) num2cell(Xyl.grRatio(idx)*100) ...
+    model.rxns(idx) model.rxnNames(idx) ...
+    constructEquations(model,idx)];
+
+mkdir([root 'data/results'])
+fid = fopen([data 'results/oleaginous.tsv'],'w');
+fprintf(fid,'%s\t%s\t%s\t%s\t%s\n',["glucose" "xylose" "rxns" "rxnName" "eqn"]);
+for j=1:length(idx)
+    fprintf(fid,'%d\t%d\t%s\t%s\t%s\n',out{j,:});
+end
+fclose(fid);
+%
+%% This script predicts metabolic engineering targets for increased
+% production of triglycerides.
+% Add exchange reactions for linolenate and triglyceride. For the
+% triglyceride, we specifically choose 16:0/18:1/18:1-TAG as target.
+idx = getIndexes(model, {'linolenate[c]','triglyceride (1-16:0, 2-18:1, 3-18:1)[erm]'}, 'metscomps');
+%
+% Add exchange reactions for products
+model = addExchangeRxns(model, 'out', idx);
+% Keep track of ids of exchange reactions
+rxn1 = model.rxns(end-1);
+rxn2 = model.rxns(end);
+%
+% Perform FSEOF for linolenic acid and TAG on glucose
+model       = setParam(model, 'eq', {'r_1714', 'r_1718'}, [-1, 0]);
+%
+targets{1} = FSEOF(model, 'r_2111', rxn1, 10, 0.9);
+targets{2} = FSEOF(model, 'r_2111', rxn2, 10, 0.9);
+%
+% Perform FSEOF for linolenic acid and TAG on xylose
+model       = setParam(model, 'eq', {'r_1714', 'r_1718'}, [0, -1]);
+%
+targets{3} = FSEOF(model, 'r_2111', rxn1, 10, 0.9);
+targets{4} = FSEOF(model, 'r_2111', rxn2, 10, 0.9);
+%
+% Summarize results in table
+geneAssoc = ~cellfun('isempty',model.grRules);
+for i=1:size(targets,2)
+    target(:,i)=targets{i}.logical;
+end
+for i=1:size(targets,2)
+    slope(:,i)=targets{i}.slope;
+    slope(~target(:,i),i)=nan;
+end
+%
+target  = find(sum(target,2) & geneAssoc);
+[~,I]=sort(sum(slope(target,:),2,'omitnan'),'descend');
+out     = [num2cell(slope(target(I),:)), model.rxnNames(target(I)), model.grRules(target(I))];
+%
+fid = fopen([data '/results/fseof_TAG_linolenicAcid.tsv'],'w');
+fprintf(fid,'%s\t%s\t%s\t%s\t%s\t%s\n',["glu_LA" "glu_TAG" "xyl_LA" "xyl_TAG" ...
+    "rxnName" "grRule"]);
+for j=1:length(I)
+    fprintf(fid,'%d\t%d\t%d\t%d\t%s\t%s\n',out{j,:});
+end
+fclose(fid);
